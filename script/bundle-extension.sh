10bdf9b9cd0adbfefb6454ade87694cdd17e1cda
mrloop <ewan@mrloop.com>
2020-08-14 18:52:49 +0100

    build: live reload of extension
    Improve build script so live reload of web extension works.

--- script/bundle-extension.sh
+++ script/bundle-extension.sh
@@ -4,25 +4,62 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const cpy = require('cpy');
+const watch = require('@cnakazawa/watch');
+const { execSync } = require('child_process');
 
-const tmp = fs.mkdtempSync(path.join(os.tmpdir(), 'race-ext-react-'));
-const buildPath = path.join('build', 'static');
+class Builder {
+  buildPath = path.join('build', 'static');
+  assetManifestPath = path.join('build', 'asset-manifest.json');
+  tmp = fs.mkdtempSync(path.join(os.tmpdir(), 'race-ext-react-'));
 
-const assetManifest = JSON.parse(fs.readFileSync('build/asset-manifest.json'));
-const extManifest = JSON.parse(fs.readFileSync('extension/manifest.json'));
+  contentByExtention(ext) {
+    const assetManifest = JSON.parse(fs.readFileSync(this.assetManifestPath));
+    return assetManifest.entrypoints.filter((filepath) => path.extname(filepath) === ext);
+  }
 
-contentByExtention = (ext) => {
-  return assetManifest.entrypoints.filter((filepath) => path.extname(filepath) === ext);
+  buildApp() {
+    execSync('yarn run react-scripts build');
+  }
+
+  buildExt() {
+    if (fs.existsSync(this.assetManifestPath)) {
+      const extManifest = JSON.parse(fs.readFileSync('extension/manifest.json'));
+      extManifest.content_scripts[0].js = this.contentByExtention('.js');
+      extManifest.content_scripts[0].css = this.contentByExtention('.css');
+      fs.writeFileSync(path.join(this.tmp, 'manifest.json'), JSON.stringify(extManifest));
+      cpy('.', path.join(this.tmp, 'static'), { parents: true, cwd: this.buildPath });
+      return this.tmp;
+    }
+  }
+
+  build() {
+    this.buildApp();
+    const dir = this.buildExt();
+    process.stdout.write(`${dir}\n`);
+  }
+
+  watch() {
+    this.watchAppSrc();
+    this.watchAppBuild();
+  }
+
+  watchAppSrc() {
+    watch.watchTree('src', () => {
+      this.buildApp();
+    })
+  }
+
+  watchAppBuild() {
+    watch.watchTree('build', { filter: (f) => {
+      return f === this.assetManifestPath;
+    }}, (f) => {
+      const sourceDir = this.buildExt();
+      process.stdout.write(`${sourceDir}\n`);
+    })
+  }
 }
 
-extManifest.content_scripts[0].js = contentByExtention('.js');
-extManifest.content_scripts[0].css = contentByExtention('.css');
+const builder = new Builder()
+fncName = process.argv[2];
+builder[fncName]();
 
-fs.writeFileSync(path.join(tmp, 'manifest.json'), JSON.stringify(extManifest));
-
-cpy('.', path.join(tmp, 'static'), { parents: true, cwd: buildPath });
-
-// doesnt work with symbolic links
-// fs.symlinkSync(path.resolve(buildPath), path.join(tmp, 'static'));
-
-process.stdout.write(tmp);
